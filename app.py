704ae8e73394f36680bdc2ddc16124854acb0816
John Lukach <john@lukach.net>
2025-03-22 00:15:04 +0000

    deploy

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 
 import aws_cdk as cdk
 
+from packages.packages_deploy import PackagesDeploy
 from packages.packages_layersuse1 import PackagesLayersUSE1
 from packages.packages_layersuse2 import PackagesLayersUSE2
 from packages.packages_layersusw2 import PackagesLayersUSW2
@@ -10,8 +11,8 @@ from packages.packages_stack import PackagesStack
 
 app = cdk.App()
 
-PackagesStack(
-    app, 'PackagesStack',
+PackagesDeploy(
+    app, 'PackagesDeploy',
     env = cdk.Environment(
         account = os.getenv('CDK_DEFAULT_ACCOUNT'),
         region = 'us-east-2'
@@ -48,6 +49,17 @@ PackagesLayersUSW2(
     env = cdk.Environment(
         account = os.getenv('CDK_DEFAULT_ACCOUNT'),
         region = 'us-west-2'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+PackagesStack(
+    app, 'PackagesStack',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-2'
     ),
     synthesizer = cdk.DefaultStackSynthesizer(
         qualifier = '4n6ir'
